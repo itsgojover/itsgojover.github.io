b6f48c43d93e801f003c9ba2f27e12fc35fc9549
adl <84267487+AdlOuz@users.noreply.github.com>
2024-06-19 19:51:57 +0300

    i dont even know

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ let selectedIndex = -1; // To keep track of the selected suggestion
 
 async function fetchPeopleData() {
     try {
-        const response = await fetch('../people.json');
+        const response = await fetch('./data/people.json');
         const data = await response.json();
         peopleData = data;
     } catch (error) {
@@ -16,62 +16,60 @@ async function fetchPeopleData() {
 function searchPeople(query) {
     const suggestionsContainer = document.getElementById('suggestions');
 
-    // Clear suggestions if the query is empty
-    if (query === '') {
+    if (query.length >= 1) {
+        const matchingPeople = peopleData.filter(person => {
+            const fullName = `${person.name} ${person.surname}`;
+            const startsWithQuery = fullName.toLowerCase().startsWith(query.toLowerCase());
+            return startsWithQuery;
+        });
+    
+        suggestionsContainer.innerHTML = '';
+    
+        if (matchingPeople.length === 0) {
+            // If there are no matches, show the 'Bulunamadi' suggestion
+            const noMatchItem = document.createElement('li');
+            noMatchItem.textContent = 'Bulunamadı';
+            suggestionsContainer.appendChild(noMatchItem);
+        } else {
+            matchingPeople.forEach((person, index) => {
+                const suggestionItem = document.createElement('li');
+                suggestionItem.value = `${person.id}`;
+                if(person.birthday)
+                {
+                    suggestionItem.textContent = `${person.name} ${person.surname ? person.surname : "(Soyadı yok)"} - ${person.birthday.substring(0,4)}`;
+                }
+                else
+                {
+                    suggestionItem.textContent = `${person.name} ${person.surname ? person.surname : "(Soyadı yok)"} - Bilinmiyor`;    
+                }
+                suggestionItem.addEventListener('click', () => selectSuggestion(person.id));
+                suggestionItem.addEventListener('mouseenter', () => highlightSuggestion(index));
+                suggestionsContainer.appendChild(suggestionItem);
+            });
+        }
+    
+        suggestionsContainer.style.display = "block";
+    }
+    else{
         suggestionsContainer.innerHTML = '';
         suggestionsContainer.style.display = "none"; // Hide the suggestions container
         selectedIndex = -1; // Reset the selected index
         return;
     }
-
-    const matchingPeople = peopleData.filter(person => {
-        const fullName = `${person.name} ${person.surname}`;
-        const startsWithQuery = fullName.toLowerCase().startsWith(query.toLowerCase());
-        return startsWithQuery;
-    });
-
-    suggestionsContainer.innerHTML = '';
-
-    if (matchingPeople.length === 0) {
-        // If there are no matches, show the 'Bulunamadi' suggestion
-        const noMatchItem = document.createElement('li');
-        noMatchItem.textContent = 'Bulunamadı';
-        suggestionsContainer.appendChild(noMatchItem);
-    } else {
-        matchingPeople.forEach((person, index) => {
-            const suggestionItem = document.createElement('li');
-            suggestionItem.value = `${person.id}`;
-            if(person.birthday)
-            {
-                suggestionItem.textContent = `${person.name} ${person.surname} - ${person.birthday.substring(0,4)}`;
-            }
-            else
-            {
-                suggestionItem.textContent = `${person.name} ${person.surname} - Bilinmiyor`;    
-            }
-            suggestionItem.addEventListener('click', () => selectSuggestion(person.id));
-            suggestionItem.addEventListener('mouseenter', () => highlightSuggestion(index));
-            suggestionsContainer.appendChild(suggestionItem);
-        });
-    }
-
-
-    suggestionsContainer.style.display = "block";
+    
 }
 
 function selectSuggestion(id) {
     const selectedPerson = peopleData.find(person => person.id === id);
-    const personDetailsBox = document.getElementById('personDetails');
     const suggestionsContainer = document.getElementById('suggestions');
 
     if (selectedPerson) {
-        const fullName = `${selectedPerson.name} ${selectedPerson.surname} - ${selectedPerson.birthday ? selectedPerson.birthday.substring(0,4) : "Bilinmiyor"}`;
+        const fullName = `${selectedPerson.name} ${selectedPerson.surname ? selectedPerson.surname : "(Soyadı yok)"}`;
         document.getElementById('search').value = fullName;
 
         displayPersonDetails(id);
-
-        // Make the personDetails box visible
-        personDetailsBox.style.display = 'flex';
+        
+        suggestionsContainer.innerHTML = ""
     } else {
         console.error('Person not found');
     }
@@ -94,18 +92,36 @@ function highlightSuggestion(index) {
 // Handle keyboard events
 document.getElementById('search').addEventListener('keydown', (event) => {
     const suggestions = document.querySelectorAll('#suggestions li');
-    if (event.key === 'ArrowDown' && selectedIndex < suggestions.length - 1) {
+    const suggestionsCount = suggestions.length;
+
+    if (event.key === 'ArrowDown') {
         event.preventDefault(); // Prevent scrolling the page
-        selectedIndex += 1;
-    } else if (event.key === 'ArrowUp' && selectedIndex > 0) {
+        selectedIndex = (selectedIndex + 1) % suggestionsCount;
+    } else if (event.key === 'ArrowUp') {
         event.preventDefault(); // Prevent scrolling the page
-        selectedIndex -= 1;
+        selectedIndex = (selectedIndex - 1 + suggestionsCount) % suggestionsCount;
     } else if (event.key === 'Enter' && selectedIndex !== -1) {
         event.preventDefault(); // Prevent submitting the form
         const suggestionsContainer = document.getElementById('suggestions');
         const listItems = suggestionsContainer.getElementsByTagName('li');
         selectSuggestion(listItems[selectedIndex].value);
     }
+    
+    const suggestionsList = document.getElementById('suggestions');
+
+    // Scroll to the highlighted suggestion 
+    const highlightedSuggestion = suggestions[selectedIndex]; 
+    const scrollTop = suggestionsList.scrollTop; 
+    const scrollHeight = suggestionsList.scrollHeight; 
+    const clientHeight = suggestionsList.clientHeight; 
+
+    // If the highlighted item is out of view, scroll
+    if (highlightedSuggestion.offsetTop + highlightedSuggestion.offsetHeight > scrollTop + clientHeight) {
+        suggestionsList.scrollTop = highlightedSuggestion.offsetTop + highlightedSuggestion.offsetHeight - clientHeight;
+    } else if (highlightedSuggestion.offsetTop < scrollTop) {
+        suggestionsList.scrollTop = highlightedSuggestion.offsetTop; 
+    }
+
     highlightSuggestion(selectedIndex);
 });
 
@@ -118,7 +134,7 @@ function displayPersonDetails(personId) {
             <tr class="personInfo"><td>Adı:</td><td>${person.name ? person.name : "Yok"}</td></tr>
             <tr class="personInfo"><td>Soyadı:</td><td>${person.surname ? person.surname : "Yok"}</td></tr>
             <tr class="personInfo"><td>Doğum tarihi:</td><td>${person.birthday ? person.birthday : "Yok"}</td></tr>
-            <tr class="personInfo"><td>${person.partners ? (person.partners.length > 1 ? 'Eşleri' : "Eşi") : "Eşi"}:</td><td>${person.partners && person.partners.length > 1 ? person.partners.join(', ') : person.partners && person.partners.length == 1 ? person.partners[0]: 'Yok'}</td></tr>
+            <tr class="personInfo"><td>${person.partners ? (person.partners.length > 1 ? 'Eşleri' : "Eşi") : "Eşi"}:</td><td>${formatClickableNames(person.partners)}</td></tr>
             <tr class="personInfo"><td>Anne - Baba:</td><td>${formatClickableNames(person.parents)}</td></tr>
             <tr class="personInfo"><td>Çocukları:</td><td>${formatClickableNames(person.children)}</td></tr>
         `;
@@ -129,9 +145,25 @@ function displayPersonDetails(personId) {
         // Attach event listeners to the clickable links
         attachClickEvents(person.parents);
         attachClickEvents(person.children);
+
+        // Make the personDetails box visible
+        showPersonDetails();
     } else {
         console.error('Person not found');
     }
+}
+
+// Function to show person details
+function showPersonDetails() {
+    const personDetailsBox = document.getElementById('personDetails');
+    personDetailsBox.style.display = 'block';
+}
+
+// Function to close person details
+function closePersonDetails() {
+    const personDetailsBox = document.getElementById('personDetails');
+    personDetailsBox.style.display = 'none';
+    document.getElementById('search').value = "";
 }
 
 // Function to format names as clickable links
@@ -148,6 +180,7 @@ function attachClickEvents(ids) {
         link.addEventListener('click', (event) => {
             event.preventDefault();
             const clickedId = event.target.dataset.id;
+            document.getElementById('search').value = getNameById(Number(clickedId));
             displayPersonDetails(Number(clickedId));
         });
     });
@@ -156,5 +189,8 @@ function attachClickEvents(ids) {
 // Function to get the name by ID
 function getNameById(id) {
     const person = peopleData.find(p => p.id === id);
-    return person ? `${person.name} ${person.surname}` : 'Unknown';
+    return person ? `${person.name} ${person.surname ? person.surname : "(Soyadı yok)"}` : 'Unknown';
 }
+
+
+
